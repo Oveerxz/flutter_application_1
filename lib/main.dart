57aea75e647dcea2b2d6eef64779d7ef00ccd796
Oveerxz <jeank.15@hotmail.com>
2024-05-30 23:47:46 -0500

    Flutter

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mi Aplicacion Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+  String _selectedOption = 'Opcion 1';
+  final List<String> _options = ['Opcion 1', 'Opcion 2', 'Oction 3'];
+
+  void _showMessage() {
+    String text = _controller.text;
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Text: $text, Option: $_selectedOption'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo Home Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Ingresa El texto',
+              ),
+            ),
+            SizedBox(height: 16),
+            DropdownButton<String>(
+              value: _selectedOption,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedOption = newValue!;
+                });
+              },
+              items: _options.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _showMessage,
+              child: Text('Mostrar Mensaje'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
